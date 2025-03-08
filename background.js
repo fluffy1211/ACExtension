de6cbcffe5cd0eec52154b804b19174cc3c3e6e3
Gabriel Martin <gabrielmartin13009@gmail.com>
2025-03-08 23:33:28 +0100

    Add Prettier plugins and update popup UI

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === "buttonClicked") {
-      console.log("Button was clicked in the popup!");
-      // You can perform background tasks here
-    }
-  });
-  
-  // Optional: Add an event for when the extension is installed
-  chrome.runtime.onInstalled.addListener(function() {
-    console.log("Extension installed!");
-  });
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "buttonClicked") {
+    console.log("Button was clicked in the popup!");
+    // You can perform background tasks here
+  }
+});
+
+// Optional: Add an event for when the extension is installed
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("Extension installed!");
+});
